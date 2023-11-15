40c7820148974a9de90b1453b10d898bd886fa47
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2023-11-15 08:15:16 +0530

    Update theark.m

--- theark.m
+++ theark.m
@@ -1,3 +1,25 @@
 %crammer's ark %
-sprintf("-----------------------Crammer's Ark-----------------------");
-sprintf("-----------------------Version 1.0-------------------------");
+
+%menu
+disp("-----------------------Crammer's Ark-----------------------");    
+disp("-----------------------Version 1.0-------------------------");
+disp("------Developed By------");
+disp("Aryan Karamtoth (SpaciousCoder78)");
+disp("------------------------Menu-------------------------------");
+disp("1.Solve Crammer's Equation");
+disp("2.Exit");
+prompt = "Enter your choice: ";
+choice = input(prompt);
+if choice == 1
+    solve()
+else 
+    disp("Exited the app");
+end
+
+
+
+function solve()
+    disp("test");
+    prompt = "Enter matrix";
+    mat = input(prompt);
+end
