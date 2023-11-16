a9bd95a592d28ec842b599617b402f6ecac82167
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2023-11-16 21:00:28 +0530

    updated a little

--- theark.m
+++ theark.m
@@ -1,25 +1,44 @@
 %crammer's ark %
 
 %menu
-disp("-----------------------Crammer's Ark-----------------------");    
-disp("-----------------------Version 1.0-------------------------");
-disp("------Developed By------");
-disp("Aryan Karamtoth (SpaciousCoder78)");
-disp("------------------------Menu-------------------------------");
-disp("1.Solve Crammer's Equation");
-disp("2.Exit");
-prompt = "Enter your choice: ";
-choice = input(prompt);
-if choice == 1
-    solve()
-else 
-    disp("Exited the app");
+
+%count variable to keep the menu loop alive
+count = 1;
+while count==1
+    %calling menu function
+    count=menu();
+end
+
+%menu function
+function menu()
+    disp("-----------------------Crammer's Ark-----------------------");    
+    disp("-----------------------Version 1.0-------------------------");
+    disp("------Developed By------");
+    disp("Aryan Karamtoth (SpaciousCoder78)");
+    disp("------------------------Menu-------------------------------");
+    disp("1.Solve Crammer's Equation");
+    disp("2.Exit");
+    %prompting the user to enter a choice
+    prompt = "Enter your choice: ";
+    %accepting user input
+    choice = input(prompt);
+    %switch case to divert the user 
+    switch choice
+        %case 1
+        case 1
+           solve();
+        %case 2
+        case 2
+            disp("Exited the app")
+
+    end
 end
 
 
-
+%solving matrix function
 function solve()
     disp("test");
     prompt = "Enter matrix";
     mat = input(prompt);
+    menu();
 end
